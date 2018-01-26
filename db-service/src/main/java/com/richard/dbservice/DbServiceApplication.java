0d106c6a2e6b285813393b19e2ae2928319890af
Ricardolv <richardluizv@gmail.com>
2018-01-26 03:20:39 -0200

    create eureka-client in project db-service

--- db-service/src/main/java/com/richard/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/richard/dbservice/DbServiceApplication.java
@@ -3,8 +3,10 @@ package com.richard.dbservice;
 import com.richard.dbservice.repository.QuotesRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableEurekaClient
 @EnableJpaRepositories(basePackageClasses = QuotesRepository.class)
 @SpringBootApplication
 public class DbServiceApplication {
